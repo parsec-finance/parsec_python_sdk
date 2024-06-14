ba62c8f04c8e1af9cf64f3dc14278f8d2e28f256
William Sheehan <willsheehan95@gmail.com>
2024-06-14 13:14:02 -0400

    0.1.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='parsecfi',
-    version='0.1.5',
+    version='0.1.6',
     description='Python SDK for the Parsec API',
     author='PARSEC FINANCE INC.',
     packages=[
